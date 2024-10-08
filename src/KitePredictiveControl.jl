9a539efde7f264eb01fcbbe472101120a745bc0c
1-Bart-1 <bart@vandelint.net>
2024-10-08 14:13:43 +0200

    working linear mpc

--- src/KitePredictiveControl.jl
+++ src/KitePredictiveControl.jl
@@ -20,8 +20,19 @@ if ! @isdefined kite
 end
 kite_model, inputs = model!(kite, kite.pos, kite.vel)
 kite_model = complete(kite_model)
-outputs = [kite_model.flap_angle[i] for i in 1:2]
-wanted_outputs = [kite.flap_angle[i] for i in 1:2]
+outputs = vcat(
+    vcat(kite_model.flap_angle), 
+    reduce(vcat, collect(kite_model.pos[:, 1:kite.num_flap_C])), 
+    reduce(vcat, collect(kite_model.pos[:, kite.num_flap_D+1:kite.num_A])),
+    vcat(kite_model.tether_length)
+)
+initial_outputs = vcat(
+    vcat(kite.flap_angle), 
+    reduce(vcat, kite.pos[1:kite.num_flap_C]), 
+    reduce(vcat, kite.pos[kite.num_flap_D+1:kite.num_A]),
+    vcat(kite.tether_lengths)
+)
+
 (f_ip, dvs, psym, io_sys) = get_control_function(kite_model, inputs)
 f!, (h!, nu, ny, nx, vu, vy, vx) = generate_f_h(io_sys, inputs, outputs, f_ip, dvs, psym)
 
@@ -33,41 +44,43 @@ f!, (h!, nu, ny, nx, vu, vy, vx) = generate_f_h(io_sys, inputs, outputs, f_ip, d
 x_0 = JuliaSimCompiler.initial_conditions(io_sys, defaults(io_sys), psym)[1]
 
 Ts = 1e-2
-N = 30
-model = setname!(NonLinModel(f!, h!, Ts, nu, nx, ny, solver=RungeKutta(4; supersample=Int(10/Ts))); u=vu, x=vx, y=vy)
+N = 300
+model = setname!(NonLinModel(f!, h!, Ts, nu, nx, ny, solver=RungeKutta(4; supersample=Int(1e5*Ts))); u=vu, x=vx, y=vy)
+setstate!(model, x_0)
 
-println("sanity check")
-u = [100, 100, -100]
-@time res = sim!(model, N, u; x_0 = x_0)
-display(plot(res, plotu=false))
+# println("sanity check")
+# u = [100, 100, -100]
+# @time res = sim!(model, N, u; x_0 = x_0)
+# display(plot(res, plotu=false))
 
 println("mpc")
-max = 500.0
+max = 100.0
 gain = 5e3
-Hp, Hc, Mwt, Nwt, Lwt = 2, 1, fill(gain, ny), fill(1/gain, nu), fill(0.0, nu)
+Hp, Hc = 100, 2
 umin, umax = fill(-max, nu), fill(max, nu)
+Mwt = fill(0.0, ny)
+control_idxs = [findfirst(x -> x == string(kite_model.tether_length[i]), model.yname) for i in 1:3]
+Mwt[control_idxs] .= gain
+Nwt = fill(1/gain, nu)
 
-println("nonlinear")
-estim = UnscentedKalmanFilter(model)
-nmpc = NonLinMPC(estim; Hp, Hc, Mwt, Nwt, Lwt, Cwt=Inf)
-# nmpc = setconstraint!(nmpc; umin=umin, umax=umax)
-using JuMP; unset_time_limit_sec(nmpc.optim)
-# unset_silent(nmpc.optim)
+println("linearize")
+# if !@isdefined linmodel
+    @time linmodel = ModelPredictiveControl.linearize(model)
+    println("sanity check")
+    u = [100, 100, -100]
+    @time res = sim!(linmodel, N, u; x_0 = x_0)
+    display(plot(res; plotx=1:3, ploty=false, plotu=false))
+# end
 
-x̂_0 = vcat(x_0, wanted_outputs)
-@show x̂_0
-@time res_ry = sim!(nmpc, N, [0.161, 0.161], plant=model, x_0 = x_0, x̂_0 = x̂_0)
-display(plot(res_ry))
+# skf = SteadyKalmanFilter(linmodel; i_ym=1:ny, σQ=fill(1e-3/nx,nx), σR=fill(1e-3,ny), nint_u=0, nint_ym=0)
+# skf = SteadyKalmanFilter(linmodel; nint_ym=0, nint_u=0)
+estim = KalmanFilter(linmodel)
+mpc = LinMPC(estim; Hp, Hc, Mwt=Mwt, Nwt=Nwt, Cwt=Inf)
+mpc = setconstraint!(mpc; umin=umin, umax=umax)
+@time res = sim!(mpc, N, initial_outputs, x_0 = x_0, lastu = [0, 0, -100.0])
+display(plot(res; plotx=1:3, ploty=false, plotu=true))
 
-
-# println("linearize")
-# α=0.01; σQ=[0.1, 1.0]; σR=[5.0]; nint_u=0; σQint_u=[0.1, 0.1, 0.1]
-# @time linmodel = ModelPredictiveControl.linearize(model; x=x_0, u=fill(0, nu))
-# skf = SteadyKalmanFilter(linmodel; nint_ym=0)
-# mpc = LinMPC(linmodel; Hp, Hc, Mwt, Nwt, Cwt=Inf)
-# nmpc = setconstraint!(nmpc; umin=umin, umax=umax)
-# res = sim!(nmpc, N, wanted_outputs.+0.01, x_0 = x_0, x̂_0 = x̂_0, lastu = [0, 0, -50.0])
-# display(plot(res))
+# xhat should be 86
 
 # println("nonlinear stepping")
 # function sim_adapt!(mpc, model, N, ry, plant, x_0, x̂_0, y_step=[0, 0])
