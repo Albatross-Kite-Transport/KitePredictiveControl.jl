3ce44138b2986da368ee2655dafe9d1282f84941
1-Bart-1 <bart@vandelint.net>
2024-10-21 08:49:46 +0200

    looking good

--- examples/simple.jl
+++ examples/simple.jl
@@ -1,28 +1,44 @@
 using Revise, KitePredictiveControl, KiteModels, ControlPlots
 
-Ts = 0.05
+Ts = 0.1
 if !@isdefined kite_real
     kite_real = KPS4_3L(KCU(se("system_3l.yaml")))
     kite_model = KPS4_3L(KCU(se("system_3l.yaml"))) # TODO: different sim vs real system
 end
 
-init_sim!(kite_real; prn=true, torque_control=false) # TODO: move to different core
-init_sim!(kite_model; prn=true, torque_control=false)
+init_set_values = [-0.1, -0.1, -70.0]
+init_sim!(kite_real; prn=true, torque_control=true, init_set_values) # TODO: move to different core
+init_sim!(kite_model; prn=true, torque_control=true, init_set_values)
 
-ci = ControlInterface(kite_model; Ts)
-init_sim!(kite_real; prn=true, torque_control=false)
+ci = ControlInterface(kite_model; Ts, init_set_values)
+# init_sim!(kite_real; prn=true, torque_control=true, init_set_values=[-10, -10, -70])
 
+function wanted_heading_y(t)
+    wanted_heading_y = 0.0
+    if t < 10.0
+        wanted_heading_y = 0.0
+    elseif t < 20.0
+        wanted_heading_y = (t-10.0) * deg2rad(1.0) / 10.0
+    else
+        wanted_heading_y = deg2rad(1.0)
+    end
+    return wanted_heading_y
+end
+
+total_time = 100
 try
     live_plot(ci)
-    for i in 1:1000
+    for i in 1:Int(div(total_time, Ts))
         start_t = time()
-        println("t = ", i*Ts-Ts)
+        t = i*Ts-Ts
+        println("t = ", t)
+        ci.wanted_outputs[KitePredictiveControl.idx(ci.linmodel.yname, ci.sys.heading_y)] = wanted_heading_y(t)
         set_values = step!(ci, kite_real.integrator)
-        next_step!(kite_real; set_values, dt = Ts)
+        real = @elapsed next_step!(kite_real; set_values, dt = Ts)
         # plot2d(kite_real.pos, (i-1)*Ts; zoom=false, front=false, xlim=(-35, 35), ylim=(0, 70))
         pause_t = Ts - (time() - start_t)
-        if (pause_t < 0) println("pause_t = ", pause_t)
-        else sleep(pause_t) end
+        if (pause_t + real < 0) println("pause_t = ", pause_t)
+        elseif (pause_t < 0) sleep(abs(pause_t)) end
     end
 finally
     sleep(2)
--- src/KitePredictiveControl.jl
+++ src/KitePredictiveControl.jl
@@ -51,7 +51,7 @@ end
 
 include("mtk_interface.jl")
 
-function ControlInterface(kite; Ts = 0.05)
+function ControlInterface(kite; Ts = 0.05, init_set_values = zeros(3))
     # --- get symbolic model ---
     sym_model, inputs = model!(kite, kite.pos, kite.vel)
     sym_model = complete(sym_model)
@@ -60,44 +60,51 @@ function ControlInterface(kite; Ts = 0.05)
         reduce(vcat, collect(sym_model.pos[:, 4:kite.num_flap_C-1])), 
         reduce(vcat, collect(sym_model.pos[:, kite.num_flap_D+1:kite.num_A])),
         vcat(sym_model.tether_length),
-        sym_model.heading_y
+        sym_model.heading_y,
+        sym_model.depower,
         # sym_model.winch_force[3]
     )
     get_y = getu(kite.integrator.sol, outputs)
     y_0 = kite.integrator[outputs]
     lin_fun, sys = ModelingToolkit.linearization_function(sym_model, inputs, outputs)
 
-    time = 5 # amount of time to be saved
+    time = 20 # amount of time to be saved
     N = Int(round(time / Ts))
     # solver = QNDF(autodiff=false)
     # kite.integrator = OrdinaryDiffEqCore.init(kite.prob, solver; dt=Ts, abstol=kite.set.abs_tol, reltol=kite.set.rel_tol, save_on=false)
-    init_sim!(kite; torque_control=kite.torque_control)
+    # init_sim!(kite; prn=true, torque_control=kite.torque_control)
 
     x_0 = deepcopy(kite.integrator.u)
     p_0 = deepcopy(kite.integrator.p)
 
-    linmodel = linearize(kite, sys, lin_fun, get_y, x_0, zeros(3), p_0, Ts)
+    linmodel = linearize(kite, sys, lin_fun, get_y, x_0, init_set_values, p_0, Ts)
     setname!(linmodel, x=string.(unknowns(sys)), u=string.(inputs), y=string.(outputs))
 
     output_idxs = vcat(
         idx(linmodel.yname, sys.heading_y),
-        # idx(linmodel.yname, sys.winch_force[3]),
+        idx(linmodel.yname, sys.depower),
         # idx(linmodel.yname, sys.tether_length[1]),
+        # idx(linmodel.yname, sys.tether_length[2]),
         idx(linmodel.yname, sys.tether_length[3]),
-        idx(linmodel.yname, sys.tether_length[2]),
     )
     observed_idxs = vcat(
         # idx(sys, sys.pos[2, kite.num_A]),
         idx(sys, sys.tether_length[1]),
         idx(sys, sys.tether_length[2]),
         idx(sys, sys.tether_length[3]),
+        idx(sys, sys.flap_angle[1]),
+        idx(sys, sys.flap_angle[2]),
         # linmodel.nx + linmodel.ny
     )
 
     Mwt = fill(0.0, linmodel.ny)
-    Mwt[idx(linmodel.yname, sys.heading_y)] = 100.0
-    Mwt[idx(linmodel.yname, sys.tether_length[3])] = 0.1
-    Nwt = fill(1.0, linmodel.nu)
+    Mwt[idx(linmodel.yname, sys.heading_y)] = 1.0
+    Mwt[idx(linmodel.yname, sys.depower)] = 1.0
+    # Mwt[idx(linmodel.yname, sys.tether_length[1])] = 0.1
+    # Mwt[idx(linmodel.yname, sys.tether_length[2])] = 0.1
+    Mwt[idx(linmodel.yname, sys.tether_length[3])] = 1.0
+    Nwt = fill(0.0, linmodel.nu)
+    Lwt = fill(0.1, linmodel.nu)
 
     σR = fill(1e-4, linmodel.ny)
     σQ = fill(1e4/linmodel.nx, linmodel.nx)
@@ -105,21 +112,23 @@ function ControlInterface(kite; Ts = 0.05)
     nint_u=fill(1, linmodel.nu)
     estim = ModelPredictiveControl.KalmanFilter(linmodel; nint_u, σQint_u, σQ, σR)
 
-    Hp_time, Hc_time = 3.0, 1.0
-    Hp, Hc = Int(round(Hp_time / Ts)), Int(round(Hc_time / Ts))
+    # Hp_time, Hc_time = 1.0, Ts
+    # Hp, Hc = Int(round(Hp_time / Ts)), Int(round(Hc_time / Ts))
+    Hp, Hc = 100, 2
     optim = JuMP.Model(HiGHS.Optimizer)
-    mpc = LinMPC(estim; Hp, Hc, Mwt, Nwt, Cwt=1e9, optim)
+    mpc = LinMPC(estim; Hp, Hc, Mwt, Nwt, Lwt, Cwt=Inf)
 
-    umin, umax = [-0.1, -0.1, -1], [0.1, 0.1, 1]
-    # Δumin, Δumax = [-0.01, -0.01, -0.1], [0.01, 0.01, 0.1]
+    umin, umax = [-20, -20, -500], [0.0, 0.0, 0.0]
+    max = 0.5
+    # Δumin, Δumax = [-max, -max, -max*10], [max, max, max*10]
     ymin = fill(-Inf, linmodel.ny)
     ymax = fill(Inf, linmodel.ny)
-    ymin[idx(linmodel.yname, sys.tether_length[1])] = x_0[idx(sys, sys.tether_length[1])] - 0.1
-    ymin[idx(linmodel.yname, sys.tether_length[2])] = x_0[idx(sys, sys.tether_length[2])] - 0.1
-    ymin[idx(linmodel.yname, sys.tether_length[3])] = x_0[idx(sys, sys.tether_length[3])] - 1.0
-    ymax[idx(linmodel.yname, sys.tether_length[1])] = x_0[idx(sys, sys.tether_length[1])] + 0.1
-    ymax[idx(linmodel.yname, sys.tether_length[2])] = x_0[idx(sys, sys.tether_length[2])] + 0.1
-    ymax[idx(linmodel.yname, sys.tether_length[3])] = x_0[idx(sys, sys.tether_length[3])] + 1.0
+    # ymin[idx(linmodel.yname, sys.tether_length[1])] = 57.0
+    # ymin[idx(linmodel.yname, sys.tether_length[2])] = 57.0
+    # ymin[idx(linmodel.yname, sys.tether_length[3])] = x_0[idx(sys, sys.tether_length[3])] - 1.0
+    # ymax[idx(linmodel.yname, sys.tether_length[1])] = x_0[idx(sys, sys.tether_length[1])] + 0.3
+    # ymax[idx(linmodel.yname, sys.tether_length[2])] = x_0[idx(sys, sys.tether_length[2])] + 0.3
+    # ymax[idx(linmodel.yname, sys.tether_length[3])] = x_0[idx(sys, sys.tether_length[3])] + 1.0
     # ymin[end] = -1000
     # ymax[end] = 1000
     setconstraint!(mpc; umin, umax, ymin, ymax)
@@ -127,7 +136,10 @@ function ControlInterface(kite; Ts = 0.05)
 
     U_data, Y_data, Ry_data, X̂_data, X_data = 
         fill(NaN, linmodel.nu, N), fill(NaN, linmodel.ny, N), fill(NaN, linmodel.ny, N), fill(NaN, linmodel.nx+linmodel.ny, N), fill(NaN, linmodel.nx, N)
-    wanted_outputs = y_0 .+ deg2rad(1.0)
+    wanted_outputs = y_0
+    wanted_outputs[idx(linmodel.yname, sys.heading_y)] = deg2rad(0.0)
+    wanted_outputs[idx(linmodel.yname, sys.depower)] = 0.45
+    # wanted_outputs[idx(linmodel.yname, sys.tether_length[3])] = 
     y_noise = fill(0.0, linmodel.ny)
 
     ci = ControlInterface(
@@ -214,7 +226,7 @@ function stop_plot(ci::ControlInterface) ci.plotting = false end
 
 function controlplot(ci::ControlInterface)
     res = SimResult(ci.mpc, ci.U_data, ci.Y_data; ci.Ry_data, ci.X̂_data, ci.X_data)
-    return Plots.plot(res; plotx=false, plotxwithx̂=ci.observed_idxs, ploty=ci.output_idxs, plotu=true)
+    return Plots.plot(res; plotx=false, plotxwithx̂=ci.observed_idxs, ploty=ci.output_idxs, plotu=true, size=(1200, 900))
 end
 
 # function init!(ci::ControlInterface)
