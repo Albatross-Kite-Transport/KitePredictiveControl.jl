bbf79a8b8e8663392fa2385227bd446305a6a62a
1-Bart-1 <bart@vandelint.net>
2024-10-29 14:28:19 +0100

    very good linearization

--- src/smartdiff.jl
+++ src/smartdiff.jl
@@ -34,17 +34,23 @@ function generate_AB(sys, nsx, nu, xname)
 
     # --- heading ---
     A_pattern[idx(sys.heading_y), idx(sys.turn_rate_y)] = 1.0
+    A_pattern[idx(sys.heading_y), nsx] = 1.0
+    B_pattern[idx(sys.heading_y), [1, 2]] .= 1.0
     A_pattern[idx(sys.turn_rate_y), nsx] = 1.0
     B_pattern[idx(sys.turn_rate_y), [1, 2]] .= 1.0
 
     # --- depower ---
     A_pattern[idx(sys.depower), idx(sys.depower_vel)] = 1.0
+    A_pattern[idx(sys.depower), nsx] = 1.0
+    B_pattern[idx(sys.depower), 1:3] .= 1.0
     A_pattern[idx(sys.depower_vel), nsx] = 1.0
     B_pattern[idx(sys.depower_vel), 1:3] .= 1.0
 
     # --- tether length ---
     for i in 1:3
         A_pattern[idx(sys.tether_length[i]), idx(sys.tether_vel[i])] = 1.0
+        A_pattern[idx(sys.tether_length[i]), nsx] = 1.0
+        B_pattern[idx(sys.tether_length[i]), i] = 1.0
         A_pattern[idx(sys.tether_vel[i]), nsx] = 1.0
         B_pattern[idx(sys.tether_vel[i]), i] = 1.0
     end
@@ -74,8 +80,6 @@ end
 
 """
 Linearize using the known sparsity pattern (no sparse matrix as this allocates more).
-Measurements in discrete-time are used to calculate coefficients for the continuous-time approximation.
-The continuous time approximation is then discretized using zero order hold.
 """
 function linearize!(ci::ControlInterface, linmodel::LinModel, x0, u0)
     return linearize!(linmodel, ci.f!, ci.h!, ci.AB, ci.AB_pattern, x0, ci.x_simple_0, u0, ci.Ts; ci.time_multiplier)
@@ -85,43 +89,45 @@ function linearize!(linmodel::LinModel, f!, h!, AB::Matrix, AB_pattern::Matrix, 
     nsx = linmodel.nx
     x_simple_plus = linmodel.buffer.x
     h!(x_simple_0, x0)
-    steering_u = [0.1, -0.1]
-    middle_u = [-5.0, 5.0]
+    steering_u = [-5.0, 5.0]
+    middle_u = [-20.0, 20.0]
     nm = length(middle_u) * nu # number of measurements
     U = zeros(nm, nu)
     X = zeros(nm, nsx)
     X_prime = zeros(nm, nsx)
     for i in 1:nu
         for j in eachindex(middle_u)
-            U[i, :] .= u0 - [i == 1 ? steering_u[j] : 0.0, i == 2 ? steering_u[j] : 0.0, i == 3 ? middle_u[j] : 0.0]
+            U[i, :] .= u0 + [i == 1 ? steering_u[j] : 0.0, i == 2 ? steering_u[j] : 0.0, i == 3 ? middle_u[j] : 0.0]
             f!(x_simple_plus, x0, U[i, :], Ts * time_multiplier)
             X[i, :] .= x_simple_0
-            X_prime[i, 1:nsx] .= (x_simple_plus .- x_simple_0) ./ Ts ./ time_multiplier
+            X_prime[i, 1:nsx] .= (x_simple_plus .- x_simple_0) / time_multiplier
         end
     end
+    U[end, :] .= u0
+    f!(x_simple_plus, x0, U[end, :], Ts)
+    X[end, :] .= x_simple_0
+    X_prime[end, 1:nsx] .= x_simple_plus .- x_simple_0
     XU = [X U]
 
     # --- solve ---
     for i in 1:nsx
         if !isempty(findall(!iszero, AB_pattern[i, nsx+1:end]))
-            nz_idxs = findall(!iszero, AB_pattern[i, :])
-            Z = XU[:, nz_idxs]
-            AB[i, nz_idxs] .= Z \ X_prime[:, i]
+            optimize_idxs = findall(!iszero, AB_pattern[i, :])
+            Z = XU[:, optimize_idxs]
+            "AB * XU = X_prime"
+            AB[i, optimize_idxs] .= Z \ X_prime[:, i]
         end
     end
-    C = I(nsx)
-    D = zeros(nsx, nu)
-    css = ss(AB[:, 1:nsx], AB[:, nsx+1:end], C, D)
-    dss = c2d(css, Ts)
-    linmodel.A .= dss.A
-    linmodel.Bu .= dss.B
-    linmodel.C .= dss.C
+    # css = ss(AB[:, 1:nsx], AB[:, nsx+1:end], C, D)
+    # dss = c2d(css, Ts)
+    linmodel.A .= AB[:, 1:nsx] .+ I(nsx)
+    linmodel.Bu .= AB[:, nsx+1:end]
 
     linmodel.uop .= u0
     linmodel.yop .= h!(linmodel.buffer.y, x0)
     linmodel.xop .= x_simple_0
-    # linmodel.fop .= f!(x_simple_plus, x0, u0, Ts)
-    linmodel.fop .= linmodel.A * x_simple_0 + linmodel.Bu * u0
+    linmodel.fop .= x_simple_plus
+    # linmodel.fop .= linmodel.A * x_simple_0 + linmodel.Bu * u0
     linmodel.x0 .= 0
     return linmodel
 end
