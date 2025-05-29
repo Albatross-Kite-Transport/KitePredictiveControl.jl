2fd0a10da32df1c3b8cded5783be9527b89b32d7
1-Bart-1 <bart@vandelint.net>
2025-05-29 18:42:10 +0200

    Idea for not using mpc

---
+++ mwe/04_no_mpc.jl
@@ -0,0 +1,9 @@
+"""
+1. Speed/force controller for power tether
+2. Length control for steering tethers
+3. Linearize model:
+    - left-right tether length difference vs heading
+    - power-steering tether length difference vs angle of attack (or directly calculate the needed depower from geometry)
+4. Find optimal tether lengths from linear model
+5. Use winch controllers to go to these setpoints
+"""
