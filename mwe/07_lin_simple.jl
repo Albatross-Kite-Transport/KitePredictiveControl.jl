40500d1533b2202304f14904b2d2f5fc808c7f05
1-Bart-1 <bart@vandelint.net>
2025-08-14 15:57:31 +0200

    Observable linmodel

--- mwe/07_lin_simple.jl
+++ mwe/07_lin_simple.jl
@@ -7,42 +7,45 @@ using ModelingToolkit
 import ModelingToolkit: t_nounits as t
 
 @variables begin
+    wing_pos(t)[1, 1:3]
+    wing_vel(t)[1, 1:3]
+    ω_b(t)[1, 1:3],
+    Q_b_w(t)[1, 1:3],
+    tether_vel(t)[1:3],
+    tether_len(t)[1:3],
+    twist_ω(t)[1:2],
+    free_twist_angle(t)[1:2]
+
     heading(t)[1]
-    turn_rate(t)[1, 1:3]
-    elevation(t)[1]
-    azimuth(t)[1]
-    elevation_vel(t)[1]
-    azimuth_vel(t)[1]
-    tether_len(t)[1:3]
-    tether_vel(t)[1:3]
     winch_force(t)[1:3]
     angle_of_attack(t)[1]
 end
-lin_outputs = [
-    # measured
-    heading[1],
-    turn_rate[1,3],
-    elevation[1],
-    azimuth[1],
-    elevation_vel[1],
-    azimuth_vel[1],
-    tether_len...,
+outputs = [
+    # copy of state for observability
+    collect(wing_vel[1,:])...,
+    collect(wing_pos[1,:])...,
+    collect(ω_b[1,:])...,
+    collect(Q_b_w[1,:])...,
     tether_vel...,
+    tether_len...,
+    twist_ω...,
+    free_twist_angle...,
 
-    # not measured
+    # stuff we want to control
+    heading[1],
     winch_force...,
     angle_of_attack[1],
 ]
-@info "Linear outputs: $lin_outputs"
+@info "Linear outputs: $outputs"
 
 set = Settings("system.yaml")
 dt = 1/set.sample_freq
 sam = SymbolicAWEModel(set, "ram")
-init!(sam; lin_outputs)
+init!(sam; outputs)
 
 simple_plant_set = Settings("system.yaml")
 simple_plant_sam = SymbolicAWEModel(simple_plant_set, "simple_ram")
-init!(simple_plant_sam; lin_outputs)
+init!(simple_plant_sam; outputs)
 
 tether_set = Settings("system.yaml")
 tether_sam = SymbolicAWEModel(tether_set, "tether")
@@ -50,7 +53,7 @@ init!(tether_sam)
 
 simple_set = Settings("system.yaml")
 simple_sam = SymbolicAWEModel(simple_set, "simple_ram")
-init!(simple_sam; lin_outputs, create_control_func=true)
+init!(simple_sam; outputs, create_control_func=true)
 
 copy_to_simple!(sam, tether_sam, simple_sam; prn=false)
 copy_to_simple!(sam, tether_sam, simple_plant_sam; prn=false)
@@ -71,13 +74,12 @@ function linearize(simple_sam)
     C = statespace.C
     @assert norm(statespace.D) ≈ 0
     linmodel = setname!(LinModel{Float64}(A, Bu, C, Bd, Dd, dt),
-                        x=string.(unknowns(simple_sam.sys)), y=string.(lin_outputs))
+                        x=string.(unknowns(simple_sam.prob.sys)), y=string.(outputs))
 
     # --- compute the nonlinear model output at operating points ---
-    y = simple_sam.get_lin_y(simple_sam.integrator)
+    y = simple_sam.simple_lin_model.get_y(simple_sam.integrator)
     # --- modify the linear model operating points ---
     linmodel.uop .= u
-    @show ny length(y)
     linmodel.yop .= y
     linmodel.xop .= simple_sam.integrator.u
     # --- compute the nonlinear model next state at operating points ---
@@ -102,10 +104,10 @@ mpc = setconstraint!(mpc; umin, umax)
 
 # function sim_adapt!(mpc, N, ry, x_0, x̂_0, y_step=zeros(ny))
 #     U_data, Y_data, Ry_data = zeros(nu, N), zeros(ny, N), zeros(ny, N)
-#     initstate!(mpc, [0], sam.get_lin_y(sam.integrator))
+#     initstate!(mpc, [0], sam.simple_lin_model.get_y(sam.integrator))
 #     setstate!(mpc, x̂_0)
 #     for i = 1:N
-#         y = sam.get_lin_y(sam.integrator) + y_step
+#         y = sam.simple_model.get_y(sam.integrator) + y_step
 #         x̂ = preparestate!(mpc, y)
 #         u = moveinput!(mpc, ry)
 #         # linmodel = linearize!(simple_sam, x̂)
