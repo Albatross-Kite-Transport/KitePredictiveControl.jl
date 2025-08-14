422959c9979a9bd430a25be29f3619ce23978c37
1-Bart-1 <bart@vandelint.net>
2025-08-14 14:53:05 +0200

    Update SymbolicAWEModels

--- mwe/07_lin_simple.jl
+++ mwe/07_lin_simple.jl
@@ -1,12 +1,48 @@
 using SymbolicAWEModels
 using ModelPredictiveControl
 using ControlSystems
+using Plots
 using UnPack
+using ModelingToolkit
+import ModelingToolkit: t_nounits as t
+
+@variables begin
+    heading(t)[1]
+    turn_rate(t)[1, 1:3]
+    elevation(t)[1]
+    azimuth(t)[1]
+    elevation_vel(t)[1]
+    azimuth_vel(t)[1]
+    tether_len(t)[1:3]
+    tether_vel(t)[1:3]
+    winch_force(t)[1:3]
+    angle_of_attack(t)[1]
+end
+lin_outputs = [
+    # measured
+    heading[1],
+    turn_rate[1,3],
+    elevation[1],
+    azimuth[1],
+    elevation_vel[1],
+    azimuth_vel[1],
+    tether_len...,
+    tether_vel...,
+
+    # not measured
+    winch_force...,
+    angle_of_attack[1],
+]
+@info "Linear outputs: $lin_outputs"
 
 set = Settings("system.yaml")
 dt = 1/set.sample_freq
-sam = SymbolicAWEModel(set, "ram") # TODO: add plant
-init!(sam)
+sam = SymbolicAWEModel(set, "ram")
+init!(sam; lin_outputs)
+
+simple_plant_set = Settings("system.yaml")
+simple_plant_sam = SymbolicAWEModel(simple_plant_set, "simple_ram")
+init!(simple_plant_sam; lin_outputs)
 
 tether_set = Settings("system.yaml")
 tether_sam = SymbolicAWEModel(tether_set, "tether")
@@ -14,15 +50,16 @@ init!(tether_sam)
 
 simple_set = Settings("system.yaml")
 simple_sam = SymbolicAWEModel(simple_set, "simple_ram")
-init!(simple_sam)
+init!(simple_sam; lin_outputs, create_control_func=true)
 
-function linearize(simple_sam, sam, tether_sam)
+copy_to_simple!(sam, tether_sam, simple_sam; prn=false)
+copy_to_simple!(sam, tether_sam, simple_plant_sam; prn=false)
+
+function linearize(simple_sam)
     # Set y to sam, copy sam to simple_sam, linearize simple sam
     # TODO: set y to sam
-    find_steady_state!(sam)
-    uop = -simple_sam.set.drum_radius .*
+    u = -simple_sam.set.drum_radius .*
         [norm(winch.force) for winch in simple_sam.sys_struct.winches]
-    copy_to_simple!(sam, tether_sam, simple_sam)
     statespace = SymbolicAWEModels.linearize!(simple_sam)
     statespace = ControlSystems.ss(statespace...)
     statespace = c2d(statespace, dt)
@@ -32,28 +69,36 @@ function linearize(simple_sam, sam, tether_sam)
     A = statespace.A
     Bu = statespace.B
     C = statespace.C
-    linmodel = LinModel{Float64}(A, Bu, C, Bd, Dd, dt)
+    @assert norm(statespace.D) ≈ 0
+    linmodel = setname!(LinModel{Float64}(A, Bu, C, Bd, Dd, dt),
+                        x=string.(unknowns(simple_sam.sys)), y=string.(lin_outputs))
 
     # --- compute the nonlinear model output at operating points ---
     y = simple_sam.get_lin_y(simple_sam.integrator)
     # --- modify the linear model operating points ---
-    linmodel.uop .= uop
+    linmodel.uop .= u
+    @show ny length(y)
     linmodel.yop .= y
     linmodel.xop .= simple_sam.integrator.u
     # --- compute the nonlinear model next state at operating points ---
-    next_step!(simple_sam; set_values=uop)
-    linmodel.fop .= simple_sam.integrator.u
+    next_step!(simple_sam; set_values=u)
+    linmodel.fop .= simple_sam.integrator.u # TODO: investigate for sudden jump in x
     # --- reset the state of the linear model ---
     linmodel.x0 .= 0 # state deviation vector is always x0=0 after a linearization
     return linmodel
 end
 
-@time linmodel = linearize(simple_sam, sam, tether_sam)
+@time linmodel = linearize(simple_sam)
 nx, ny, nu = linmodel.nx, linmodel.ny, linmodel.nu
 # α=0.01; σQ=[0.1, 1.0]; σR=[5.0]; nint_u=[1]; σQint_u=[0.1]
-# estim = KalmanFilter(linmodel; σQ, σR, nint_u, σQint_u)
-# mpc = LinMPC(estim; Hp, Hc, Mwt, Nwt, Cwt=Inf)
-# mpc = setconstraint!(mpc; umin, umax)
+estim = KalmanFilter(linmodel)
+
+Hp, Hc = 20, 2
+# Mwt, Nwt = [0.5], [2.5]
+mpc = LinMPC(estim; Hp, Hc, Cwt=Inf)
+umin = fill(-100, 3)
+umax = fill(100, 3)
+mpc = setconstraint!(mpc; umin, umax)
 
 # function sim_adapt!(mpc, N, ry, x_0, x̂_0, y_step=zeros(ny))
 #     U_data, Y_data, Ry_data = zeros(nu, N), zeros(ny, N), zeros(ny, N)
--- src/KitePredictiveControl.jl
+++ src/KitePredictiveControl.jl
@@ -4,115 +4,46 @@
 
 module KitePredictiveControl
 
-using Plots
-using SymbolicAWEModels, KiteUtils, LinearAlgebra
-using ModelPredictiveControl, ModelingToolkit, OrdinaryDiffEq,
-    SymbolicIndexingInterface
-using ModelingToolkit: setu, setp, getu, getp, @variables
-using ModelingToolkit: t_nounits as t
-using SciMLBase: successful_retcode
-using RobustAndOptimalControl
+using PrecompileTools
+using SymbolicAWEModels
+using ModelPredictiveControl
 using ControlSystems
-using Printf
+using Plots
+using UnPack
+using ModelingToolkit
+import ModelingToolkit: t_nounits
 
-export linearize
+@setup_workload begin
+    @compile_workload begin
+        @variables begin
+            heading(t_nounits)[1]
+            angle_of_attack(t_nounits)[1]
+            tether_len(t_nounits)[1:3]
+            winch_force(t_nounits)[1:3]
+        end
+        lin_outputs = [heading[1], angle_of_attack[1], tether_len[1], winch_force[1]]
+        @info "Linear outputs: $lin_outputs"
 
-include("utils.jl")
-include("plotting.jl")
+        set = Settings("system.yaml")
+        dt = 1/set.sample_freq
+        sam = SymbolicAWEModel(set, "ram")
+        init!(sam; lin_outputs)
 
-function sim!(s::SymbolicAWEModel, p::ModelParams)
-    N = Int(round(time÷dt))
-    vsm_interval = Int(round(vsm_dt÷dt))
-    plot_interval = Int(round(plot_dt÷dt))
-end
+        simple_plant_set = Settings("system.yaml")
+        simple_plant_sam = SymbolicAWEModel(simple_plant_set, "simple_ram")
+        init!(simple_plant_sam; lin_outputs)
 
-function set_measured!(s::SymbolicAWEModel, p::ModelParams, x)
-    # get variables from y
-    elevation       = x[1]
-    elevation_vel   = x[2]
-    azimuth         = x[3]
-    azimuth_vel     = x[4]
-    heading         = x[5]
-    turn_rate       = x[6]
-    tether_length   = x[7:9]
-    tether_vel      = x[10:12]
+        tether_set = Settings("system.yaml")
+        tether_sam = SymbolicAWEModel(tether_set, "tether")
+        init!(tether_sam)
 
-    # get variables from integrator
-    distance = get_distance(integ)
-    R_t_w = KiteModels.calc_R_t_w(elevation, azimuth) # rotation of tether to world, similar to view rotation, but always pointing up
-    
-    # get wing_pos, rotate it by elevation and azimuth around the x and z axis
-    wing_pos = R_t_w * [0, 0, distance]
-    # wing_vel from elevation_vel and azimuth_vel
-    wing_vel = R_t_w * [-elevation_vel, azimuth_vel, tether_vel[1]]
-    # find quaternion orientation from heading, R_cad_body and R_t_w
-    x = [cos(-heading), -sin(-heading), 0]
-    y = [sin(-heading),  cos(-heading), 0]
-    z = [0, 0, 1]
-    R_b_w = R_t_w * s.wing.R_cad_body' * [x y z]
-    Q_b_w = rotation_matrix_to_quaternion(R_b_w)
-    # adjust the turn rates for observed turn rate
-    ω_b = R_b_w' * R_t_w * [0, 0, turn_rate]
-    # directly set tether length
-    # directly set tether vel
-    set_state(integ, [wing_pos, wing_vel, Q_b_w, ω_b, tether_length, tether_vel])
-    return nothing
-end
+        simple_set = Settings("system.yaml")
+        simple_sam = SymbolicAWEModel(simple_set, "simple_ram")
+        init!(simple_sam; lin_outputs)
 
-function jacobian(f::Function, x::AbstractVector, ϵ::AbstractVector)
-    n = length(x)
-    fx = f(x)
-    m = length(fx)
-    J = zeros(m, n)
-    for i in 1:n
-        x_perturbed = copy(x)
-        x_perturbed[i] += ϵ[i]
-        J[:, i] = (f(x_perturbed) - fx) / ϵ[i]
+        copy_to_simple!(sam, tether_sam, simple_sam; prn=false)
+        copy_to_simple!(sam, tether_sam, simple_plant_sam; prn=false)
     end
-    return J
-end
-
-function linearize!(s::SymbolicAWEModel, p::ModelParams, A, B, C, D)
-    integ = s.integrator
-    lin_x0 = get_y(integ)[5:12]
-    lin_u0 = get_u(integ)
-    KiteModels.linearize_vsm!(s)
-    A .= 0.0
-    B .= 0.0
-    C .= 0.0
-    D .= 0.0
-    
-    function f(x, u)
-        set_x(integ, x0)
-        sphere_pos_vel = get_sphere_pos_vel(integ)
-        set_measured!(s, [sphere_pos_vel; x])
-        set_u(integ, u)
-        OrdinaryDiffEq.reinit!(integ, integ.u; reinit_dae=false)
-        OrdinaryDiffEq.step!(integ, dt)
-        return get_lin_dx(integ)
-    end
-
-    # yes it looks weird to step in an output function, but this is a steady state finder rather than output
-    function h(x)
-        set_x(integ, x0)
-        sphere_pos_vel = get_sphere_pos_vel(integ)
-        set_measured!(s, [sphere_pos_vel; x])
-        OrdinaryDiffEq.reinit!(integ, integ.u; reinit_dae=false)
-        OrdinaryDiffEq.step!(integ, dt)
-        return get_lin_y(integ)
-    end
-
-    f_x(x) = f(x, lin_u0)
-    f_u(u) = f(lin_x0, u)
-
-    # calculate jacobian
-    ϵ_x = [0.01, 0.01, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
-    ϵ_u = [1.0, 0.1, 0.1]
-    A .= jacobian(f_x, lin_x0, ϵ_x)
-    B .= jacobian(f_u, lin_u0, ϵ_u)
-    C .= jacobian(h,   lin_x0, ϵ_x)
-
-    nothing
 end
 
 end
