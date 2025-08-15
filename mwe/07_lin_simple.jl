33646da74f541941a2a85addd57de03138adad0a
1-Bart-1 <bart@vandelint.net>
2025-08-15 20:59:34 +0200

    Should move to module

--- mwe/07_lin_simple.jl
+++ mwe/07_lin_simple.jl
@@ -44,26 +44,30 @@ outputs = [
     azimuth[1],
     tether_len...,
     tether_vel...,
+
+    winch_force[1],
+    angle_of_attack[1],
 ]
 @info "Outputs: $outputs"
 
-set = Settings("system.yaml")
-dt = 1/set.sample_freq
-sam = SymbolicAWEModel(set, "ram")
+if !@isdefined simple_sam
+    set = Settings("system.yaml")
+    dt = 1/set.sample_freq
+    sam = SymbolicAWEModel(set, "ram")
+    plant_set = Settings("system.yaml")
+    plant_sam = SymbolicAWEModel(plant_set, "ram")
+    tether_set = Settings("system.yaml")
+    tether_sam = SymbolicAWEModel(tether_set, "tether")
+    simple_set = Settings("system.yaml")
+    simple_sam = SymbolicAWEModel(simple_set, "simple_ram")
+end
+
 init!(sam; outputs)
-
-plant_set = Settings("system.yaml")
-plant_sam = SymbolicAWEModel(plant_set, "ram")
 init!(plant_sam; outputs)
-
-tether_set = Settings("system.yaml")
-tether_sam = SymbolicAWEModel(tether_set, "tether")
 init!(tether_sam)
-
-simple_set = Settings("system.yaml")
-simple_sam = SymbolicAWEModel(simple_set, "simple_ram")
 init!(simple_sam; outputs, create_control_func=true)
 
+find_steady_state!(sam; t=10, dt=3)
 copy_to_simple!(sam, tether_sam, simple_sam; prn=false)
 
 function generate_f_h(simple_sam)
@@ -130,21 +134,21 @@ end
 function preparestate!(estim::SAMEstim, y::Vector{<:Real})
     @unpack sam, simple_sam, tether_sam, get_x = estim
 
+    update_sam = sam
     # update sam with y
-    sam.sys_struct.transforms[1].heading = y[1]
-    sam.sys_struct.transforms[1].elevation = y[2]
-    sam.sys_struct.transforms[1].azimuth = y[3]
-    SymbolicAWEModels.reposition!(sam.sys_struct.transforms, sam.sys_struct)
+    update_sam.sys_struct.transforms[1].heading = y[1]
+    update_sam.sys_struct.transforms[1].elevation = y[2]
+    update_sam.sys_struct.transforms[1].azimuth = y[3]
+    SymbolicAWEModels.reposition!(update_sam.sys_struct.transforms, update_sam.sys_struct)
     i = 4
-    for winch in sam.sys_struct.winches
+    for winch in update_sam.sys_struct.winches
         winch.tether_len = y[i]
         i += 1
     end
-    for winch in sam.sys_struct.winches
+    for winch in update_sam.sys_struct.winches
         winch.tether_vel = y[i]
         i += 1
     end
-    @assert i-1 == length(y)
 
     # copy sam to simple sam
     copy_to_simple!(sam, tether_sam, simple_sam; prn=false)
@@ -153,22 +157,26 @@ function preparestate!(estim::SAMEstim, y::Vector{<:Real})
 end
 
 function updatestate!(estim::SAMEstim, u::Vector{<:Real}, y::Vector{<:Real})
-    sam = estim.sam
+    @unpack simple_sam, sam = estim
+    next_step!(simple_sam; set_values=u)
     next_step!(sam; set_values=u)
     nothing
 end
 
-function updatestate!(sam::SymbolicAWEModel, u::Vector{<:Real})
-    next_step!(sam; set_values=u)
+function updatestate!(plant_sam::SymbolicAWEModel, u::Vector{<:Real})
+    next_step!(plant_sam; set_values=u)
     nothing
 end
 
-estim = SAMEstim(sam, simple_sam, tether_sam)
+if !@isdefined estim
+    estim = SAMEstim(sam, simple_sam, tether_sam)
+end
 
 Hp, Hc = 10, 2
 Mwt = zeros(ny)
-Mwt[1] = 1.0
-Nwt = fill(0.1, nu)
+Mwt[1] = 10.0
+Mwt[11] = 10.0
+Nwt = fill(0.01, nu)
 mpc = NonLinMPC(man_estim; Hp, Hc, Mwt, Nwt, Cwt=Inf)
 umin = fill(-100, 3)
 umax = fill(100, 3)
@@ -180,18 +188,17 @@ function man_sim!(mpc, N, ry, y0, u0, x0)
     setstate!(mpc, x0)
     for i = 1:N
         @show i
-        y = plant_sam.simple_lin_model.get_y(sam.integrator)
+        y = plant_sam.simple_lin_model.get_y(plant_sam.integrator)
         # during preparestate:
         # use reposition! on complex model, update tether len/vel and tune the simple model
         @time x̂ = preparestate!(estim, y)
         ŷ = y # TODO: add a kalman filter to remove noise from y and improve ŷ.
               # this kalman filter should be added inside the custom estim, before
               # running reposition!
-        @time setstate!(mpc, x̂)
+        setstate!(mpc, x̂)
         @time u = moveinput!(mpc, ry)
         U_data[:,i], Y_data[:,i], Ry_data[:,i] = u, y, ry
         # during updatestate, step with
-        ModelPredictiveControl.updatestate!
         @time updatestate!(estim, u, y) # in the estim: step the complex model
         updatestate!(plant_sam, u)  # update plant simulator
     end
@@ -207,4 +214,4 @@ ry = copy(y0)
 ry[1] += deg2rad(10)
 u0 = sam.prob.get_set_values(sam.integrator)
 res = man_sim!(mpc, 100, ry, y0, u0, x0)
-plot(res; ploty=[1,2,3])
+plot(res; ploty=[1,10,11])
